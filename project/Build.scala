ec23bcbda69e56aba23ace84b13c60e9cc37db1b
Eugene Burmako <xeno.by@gmail.com>
2012-12-25 00:03:27 +0100

    org.scala-lang.macro-paradise!

--- project/Build.scala
+++ project/Build.scala
@@ -5,13 +5,10 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalamacros",
     version := "1.0.0",
-    scalaVersion := "2.10.0",
     scalacOptions ++= Seq(),
-    // follow the instructions at http://scalamacros.org/download.html to obtain a build of macro paradise
-    // make sure you use SBT 0.13, because otherwise you'll have problems with https://github.com/harrah/xsbt/issues/626
-    // upd: actually, SBT 0.12.2-RC1 is good enough
-    scalaHome := Some(file("/Users/xeno_by/Projects/Kepler_typemacros/build/pack")),
-    unmanagedBase := file("/Users/xeno_by/Projects/Kepler_typemacros/build/pack/lib")
+    scalaVersion := "2.10.1-SNAPSHOT",
+    scalaOrganization := "org.scala-lang.macro-paradise",
+    resolvers += Resolver.sonatypeRepo("snapshots")
   )
 }
 
@@ -28,7 +25,7 @@ object MyBuild extends Build {
     "macros",
     file("macros"),
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
+      libraryDependencies <+= (scalaVersion)("org.scala-lang.macro-paradise" % "scala-reflect" % _))
   )
 
   lazy val core: Project = Project(
